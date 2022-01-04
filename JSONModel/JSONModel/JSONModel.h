3b9ac1a71c381c7ae5a4734b9c7f25b7ccbe8933
Marcus Mattsson <marcus@shape.dk>
2022-01-04 12:56:35 +0100

    Tentatively tries to fix "target 'JSONModel' has invalid header layout: umbrella header found at X, but more than one directory exists next to its parent directory"

--- JSONModel/JSONModel/JSONModel.h
+++ JSONModel/JSONModel/JSONModel.h
@@ -6,8 +6,8 @@
 #import <Foundation/Foundation.h>
 
 #import "JSONModelError.h"
-#import <JSONValueTransformer.h>
-#import <JSONKeyMapper.h>
+#import "JSONValueTransformer.h"
+#import "JSONKeyMapper.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 #if TARGET_IPHONE_SIMULATOR
